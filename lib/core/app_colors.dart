2a632175ad621bdaed093cedcfb6b287722f2967
depot-data-thix-central <datathix@gmail.com>
2026-08-16 06:27:44 +0300

    Create app_colors.dart

---
+++ lib/core/app_colors.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const primary = Color(0xFF0D2CC1);
+  static const primaryDeep = Color(0xFF0A1E8A);
+  static const bg = Color(0xFFF4F6FA);
+  static const white = Color(0xFFFFFFFF);
+  static const textMain = Color(0xFF10192E);
+  static const textSecondary = Color(0xFF8492AC);
+  static const border = Color(0xFFE5E9F0);
+  static const danger = Color(0xFFDC2626);
+  static const success = Color(0xFF22C55E);
+  static const enterprise = Color(0xFF111827);
+}
