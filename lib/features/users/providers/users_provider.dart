52d39e4642416dd03aa3bb8c8a8f8a2c1b8aa9b2
depot-data-thix-central <datathix@gmail.com>
2026-09-11 21:11:15 +0300

    Update users_provider.dart

--- lib/features/users/providers/users_provider.dart
+++ lib/features/users/providers/users_provider.dart
@@ -3,7 +3,8 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
 
 import '../../../supabase/supabase_config.dart';
-import 'package:features/users/models/admin_user_profile.dart';
+import 'package:thix_admin/features/users/models/admin_user_profile.dart';
+
 
 /// 📦 État
 @immutable
