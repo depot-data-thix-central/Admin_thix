e9c6d196087ec6e3fda45491cdb9fab468c7ab3f
depot-data-thix-central <datathix@gmail.com>
2026-09-11 21:49:58 +0300

    Update admin_shell_page.dart

--- lib/shell/admin_shell_page.dart
+++ lib/shell/admin_shell_page.dart
@@ -5,7 +5,7 @@ import 'package:thix_admin/features/certifications/pages/admin_enterprise_certif
 import 'package:thix_admin/features/dashboard/pages/admin_dashboard_page.dart';
 import 'package:thix_admin/features/articles/pages/admin_articles_page.dart'; 
 import 'package:thix_admin/features/users/pages/admin_users_page.dart';
-
+import 'package:thix_admin/features/security/pages/admin_security_page.dart';
 
 
 class AdminModule {
@@ -48,6 +48,11 @@ class _AdminShellPageState extends State<AdminShellPage> {
       title: 'Articles & Annonces',
       icon: Icons.article_rounded,
       builder: (_) => const AdminArticlesPage(),
+    ),
+    AdminModule(
+      title: 'Sécurité',
+      icon: Icons.shield_rounded,
+      builder: (_) => const AdminSecurityPage(),
     ),
     // Prochains modules à ajouter ici, ex :
     // AdminModule(title: 'Articles', icon: Icons.article_rounded, builder: (_) => const AdminArticlesPage()),
