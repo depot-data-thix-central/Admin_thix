bcff53793dae01e82f47fb0f27fc4d5ef736054a
depot-data-thix-central <datathix@gmail.com>
2026-09-11 21:12:11 +0300

    Update user_detail_page.dart

--- lib/features/users/pages/user_detail_page.dart
+++ lib/features/users/pages/user_detail_page.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 import '../../../core/app_colors.dart';
-import 'package:features/users/models/admin_user_profile.dart';
+import 'package:thix_admin/features/users/models/admin_user_profile.dart';
 import '../providers/users_provider.dart';
 import '../widgets/user_status_badge.dart';
 
